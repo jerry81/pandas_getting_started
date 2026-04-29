e7c82e885910f9faa7879f72debb462fcb8f5ba2
Raskolnikov <jerry.tann@endress.com>
2026-04-29 12:36:08 +0800

    📝 w3 schools!

--- 01-create-read-write/create_read_write.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-pd.set_option('display.max_rows', 5)
-from learntools.core import binder; binder.bind(globals())
-from learntools.pandas.creating_reading_and_writing import *
-print("Setup complete.")
