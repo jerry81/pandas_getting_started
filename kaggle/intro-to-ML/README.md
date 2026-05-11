5027fe2be5f666183cd22c58c7353eab362f3d92
Raskolnikov <jerry.tann@endress.com>
2026-05-11 15:40:14 +0800

    📝

--- kaggle/intro-to-ML/README.md
+++ kaggle/intro-to-ML/README.md
@@ -8,3 +8,49 @@
 ## your first machine learning model
 - pare down data to something understandable
 - df.columns - see list of all columns
+- using sklearn for traditional models
+- steps - find target column (the column you predict) aka y
+- find your selector for the model (a bunch of columns) X
+- initialize model
+- fit(X,y)
+- model.predict(some rows with subset of columns)
+
+## model validation
+- this step is to improve models iteratively
+- question: how to measure model quality (predictive accuracy)
+  - error = actual - predicted
+  - MAE - mean absolute error - average of all the errors in a set of rows
+- reminder - y is actual values, predicted is .predict(x) results
+```
+mean_absolute_error(y, predicted_home_prices)
+```
+- key concept for things like decision tree - try to use different data for testing error than training data
+- use train_test_split to acheive this
+```
+train_X, val_x, train_y, val_y = train_test_split(X, y, random_state=0)
+```
+
+- then you use train_x, train_y to fit
+- then predictions use val_x
+- finally, mean_absolute_error use result of val_x predictions and val_y
+
+## underfitting, overfitting
+
+- decision tree - many options can be put in - including tree depth
+- in practice common to have 10 splits
+- overfitting - when a leaf in the tree has too few examples, it will not do well with new data, and will contain data too close to training data
+
+- if too few splits are in the tree, groups have huge variety of houses, resulting in underfitting - may even perform poorly in training data
+
+- so to find the best balance, test mean absolute error, experiment with max_leaf_nodes as input
+
+- function to find mae based on max nodes as input
+```py
+def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
+    model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
+    model.fit(train_X, train_y)
+    preds_val = model.predict(val_X)
+    mae = mean_absolute_error(val_y, preds_val)
+    return(mae)
+```
+
--- kaggle/intro-to-ML/intro-ml.py
+++ kaggle/intro-to-ML/intro-ml.py
@@ -27,8 +27,64 @@ melbourne_model.fit(X,y)
 
 print("Making predictions for the following 5 houses:")
 print(X.head())
+print("whose prices are")
+print(y.head())
 print("The predictions are")
+# predict takes in X, which are the features, and outputs the predicted target column values
 print(melbourne_model.predict(X.head()))
 
 # dt vs rf
 # dt is a single tree, rf is colleciton of trees with random subset of features
+
+####### part 2: validation #######
+
+from sklearn.metrics import mean_absolute_error
+print("mean using same train/test rows")
+train_predictions = melbourne_model.predict(X)
+print(mean_absolute_error(y, train_predictions))
+
+train_errors = (y - train_predictions).abs()
+print("max absolute error on training rows")
+print(train_errors.max())
+print("number of rows with non-zero error")
+print((train_errors > 0).sum())
+print("first rows where prediction differs from actual")
+debug_df = pd.DataFrame({
+	'actual': y,
+	'predicted': train_predictions,
+	'abs_error': train_errors,
+})
+print(debug_df[debug_df['abs_error'] > 0].head())
+
+from sklearn.model_selection import train_test_split
+
+
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=0)
+melbourne_model.fit(train_X, train_y)
+val_predictions = melbourne_model.predict(val_X)
+print("mean using split test/train")
+print(mean_absolute_error(val_y, val_predictions))
+
+####### part 3: under and overfitting #######
+
+def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
+    model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
+    model.fit(train_X, train_y)
+    preds_val = model.predict(val_X)
+    mae = mean_absolute_error(val_y, preds_val)
+    return(mae)
+
+candidate_max_leaf_nodes = [5, 25, 50, 100, 250, 500]
+import sys
+# Write loop to find the ideal tree size from candidate_max_leaf_nodes
+best_size = 5
+mx = sys.float_info.max
+for mx_ln in candidate_max_leaf_nodes:
+    mae =  get_mae(mx_ln, train_X, val_X,train_y, val_y)
+    if mae < mx:
+        best_size = mx_ln
+        mx = mae
+
+
+# Store the best value of max_leaf_nodes (it will be either 5, 25, 50, 100, 250 or 500)
+best_tree_size = best_size
