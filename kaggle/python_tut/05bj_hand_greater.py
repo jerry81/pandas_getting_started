c8421260d6f11177a9bbff585989ef2c6239254f
Raskolnikov <jerry.tann@endress.com>
2026-05-26 15:34:06 +0800

    ✨

--- kaggle/python_tut/05bj_hand_greater.py
+++ kaggle/python_tut/05bj_hand_greater.py
@@ -1,12 +1,27 @@
-def blackjack_hand_greater_than(hand_1, hand_2):
-    hand1total = 0
+def calc_highest_hand(hand):
+    handtotal = 0
     acecount = 0
-    for item in hand_1:
+    for item in hand:
         if item == 'A':
-            acecount+=1
+            acecount += 1
         elif item in ['J', 'Q', 'K']:
-            hand1total+=10
+            handtotal += 10
         else:
-            hand1total+=item
-    print("hand1total: ", hand1total)
-    pass
+            handtotal += int(item)
+
+    handtotal += acecount
+    if acecount > 0 and handtotal + 10 <= 21:
+        handtotal += 10
+
+    return handtotal
+
+def blackjack_hand_greater_than(hand_1, hand_2):
+    hand1total = calc_highest_hand(hand_1)
+    hand2total = calc_highest_hand(hand_2)
+
+    if hand1total > 21:
+        return False
+    if hand2total > 21:
+        return True
+
+    return hand1total > hand2total
